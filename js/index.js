1855fc0fa1aa8b956e978ba550c5d076509176ce
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-17 09:36:32 -0300

    Adicionado validação

--- js/index.js
+++ js/index.js
@@ -5,7 +5,30 @@ var tdAltura = paciente.querySelector(".info-altura");
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+
 var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+
+
+// validação
+
+var alturaEhValida = true;
+var pesoEhvalido = true;
+
+if (peso <= 0 || peso > 600) {
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido";
+    pesoEhvalido = false;
+}
+
+if (altura <= 0 || altura > 3) {
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida";
+    alturaEhValida = false;
+}
+
+if (alturaEhValida && pesoEhvalido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
 
