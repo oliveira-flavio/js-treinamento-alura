0fc64ba30c8373d3d4597bd55f9af1bc62c8318b
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-17 11:41:50 -0300

    Adicionado form com captação de dadas usando js.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
 
-    var tdPeso = paciente.querySelector(".info-pso");
+    var tdPeso = paciente.querySelector(".info-peso");
     var tdAltura = paciente.querySelector(".info-altura");
 
     var peso = tdPeso.textContent;
@@ -36,3 +36,38 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (Event) {
+    Event.preventDefault(); // consegue parar um comportamento padrão do html
+
+    var form = document.querySelector("#form-adiciona"); //selecionar o form
+
+    // coletar dados do form
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr"); // criar tag html
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // função para adicionar as TDs como filhos de TR (appendChild)
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+})
