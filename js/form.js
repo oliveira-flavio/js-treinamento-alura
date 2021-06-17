73230e67b036bbad837a5d8875064c4aff879589
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-17 11:54:44 -0300

    Separação de js de acordo com responsabilidade

--- js/form.js
+++ js/form.js
@@ -1,41 +1,3 @@
-var pacientes = document.querySelectorAll(".paciente");
-
-for (var i = 0; i < pacientes.length; i++) {
-
-    var paciente = pacientes[i];
-
-    var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
-
-    var peso = tdPeso.textContent;
-    var altura = tdAltura.textContent;
-
-    var tdImc = paciente.querySelector(".info-imc");
-
-    // validação
-
-    var alturaEhValida = true;
-    var pesoEhvalido = true;
-
-    if (peso <= 0 || peso > 600) {
-        console.log("Peso inválido");
-        tdImc.textContent = "Peso inválido";
-        pesoEhvalido = false;
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (altura <= 0 || altura > 3) {
-        console.log("Altura inválida");
-        tdImc.textContent = "Altura inválida";
-        alturaEhValida = false;
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (alturaEhValida && pesoEhvalido) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
-}
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (Event) {
     Event.preventDefault(); // consegue parar um comportamento padrão do html
@@ -69,5 +31,4 @@ botaoAdicionar.addEventListener("click", function (Event) {
 
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
-
 })
