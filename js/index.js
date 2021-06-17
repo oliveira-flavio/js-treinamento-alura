4171ae9e683c2ea44737adf9c22a864cd3b75927
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-17 09:15:47 -0300

    Manipulaçao dos elementos com Javascript

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-pso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
