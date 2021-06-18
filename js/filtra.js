e679d0de8201f9d7457ce70d5b26c76e3709cb42
Flavio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-18 15:01:55 -0300

    Adicionado requisição api get

--- js/filtra.js
+++ js/filtra.js
@@ -1,8 +1,9 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function () {
-    console.log(this.value);
+
     var pacientes = document.querySelectorAll(".paciente");
+
     if (this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
--- js/form.js
+++ js/form.js
@@ -7,8 +7,6 @@ botaoAdicionar.addEventListener("click", function (Event) {
     // coletar dados do form
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
 
     if (erros.length > 0) {
@@ -16,15 +14,20 @@ botaoAdicionar.addEventListener("click", function (Event) {
         exibeMensagensDeErro(erros);
         return;
     }
-
     // adiciona o paciente na tabela
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 })
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros) {
     var ul = document.querySelector("#mensagens-erro");
